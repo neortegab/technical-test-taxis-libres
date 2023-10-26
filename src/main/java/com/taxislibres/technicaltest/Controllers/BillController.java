b9e838607e865c4c53de8c88c32965aa96ce516e
Nelson Ortega <ne.beltran18@gmail.com>
2023-10-25 23:10:00 -0500

    feat: mapping User and Bill controllers

--- src/main/java/com/taxislibres/technicaltest/Controllers/BillController.java
+++ src/main/java/com/taxislibres/technicaltest/Controllers/BillController.java
@@ -1,5 +1,10 @@
 package com.taxislibres.technicaltest.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/bills")
 public class BillController {
 
 }
--- src/main/java/com/taxislibres/technicaltest/Controllers/UserController.java
+++ src/main/java/com/taxislibres/technicaltest/Controllers/UserController.java
@@ -1,4 +1,10 @@
 package com.taxislibres.technicaltest.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/users")
 public class UserController {
 
 }
