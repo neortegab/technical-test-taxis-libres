676f62814bf274d7b4b183e4505c3fb955c0bf74
Nelson Ortega <ne.beltran18@gmail.com>
2023-10-30 21:16:56 -0500

    feat jwt: manual JWT implemented. Need to correct issues, probably with Spring Security JWT applicance

--- src/main/java/com/taxislibres/technicaltest/Security/SecurityConfig.java
+++ src/main/java/com/taxislibres/technicaltest/Security/SecurityConfig.java
@@ -10,22 +10,30 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
-    private UserService userService;
+    private final JwtAuthEntryPoint authEntryPoint;
 
     @Autowired
-    public SecurityConfig(UserService userService){ this.userService = userService; }
+    public SecurityConfig(JwtAuthEntryPoint authEntryPoint){
+        this.authEntryPoint = authEntryPoint;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
-                .csrf(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         authorize -> authorize
                                 .requestMatchers("/auth/**").permitAll()
@@ -33,11 +41,14 @@ public class SecurityConfig {
                 )
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults());
+        httpSecurity.addFilterBefore(new JwtAuthFilter(), UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
     }
 
